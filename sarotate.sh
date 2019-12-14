0186dcd3b209a250f2fb7e95b91a41e003b006b1
Visorask <54461452+Visorask@users.noreply.github.com>
2019-12-14 16:58:36 -0600

    Update sarotate.sh

--- sarotate.sh
+++ sarotate.sh
@@ -3,10 +3,10 @@
 ## Variables
 # Name of remote drive here
 REMOTE=remotename
-# First json file number in your JSON directory.     
+# First json file number in your JSON directory.  
 MINJS=start#
-# Max json file number you wish to use in your JSON directory.         
-MAXJS=end# 
+# Max json file number you wish to use in your JSON directory.     
+MAXJS=end#
 # Location of DIR with SA .json files.
 JSONDIR=/your/dir/here
 # Amount of time you would like to have the script sleep before switching SA .json files.
@@ -17,10 +17,10 @@ COUNT=$MINJS
 # Adjusts from rclone config remote syntax to export syntax here. Makes $REMOTE all caps.
 REMOTE=`sed 's/-/_/g' <<<${REMOTE^^}`
 
-while : 
+while :
 do
 	echo SA Rotate is running.
-		COUNT=$(( $COUNT>=$MAXJS?MINJS:$COUNT+1 )) 
+		COUNT=$(( $COUNT>=$MAXJS?MINJS:$COUNT+1 ))
 		export RCLONE_CONFIG_$REMOTE_SERVICE_ACCOUNT_FILE=$JSONDIR/$COUNT.json
 	echo SA changed to $JSONDIR/$COUNT.JSON
 	echo SA rotate is now going to sleep for $SLEEPTIME
